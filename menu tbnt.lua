6968f165796e83fc944a44a2b3804c9a9ae61a6c
tototott <buirethimote02@gmail.com>
2026-01-13 22:08:50 +0100

    Update menu tbnt.lua

--- menu tbnt.lua
+++ menu tbnt.lua
@@ -71,7 +71,7 @@ end
 
 Menu.Banner = {
     enabled = true,
-    imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png",
+    imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png",
     height = 100
 }
 
@@ -143,27 +143,27 @@ function Menu.ApplyTheme(themeName)
     if themeLower == "red" then
         Menu.Colors.HeaderPink = { r = 255, g = 0, b = 0 }
         Menu.Colors.SelectedBg = { r = 255, g = 0, b = 0 }
-        Menu.Banner.imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png"
+        Menu.Banner.imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png"
         Menu.CurrentTheme = "Red"
     elseif themeLower == "purple" then
         Menu.Colors.HeaderPink = { r = 148, g = 0, b = 211 }
         Menu.Colors.SelectedBg = { r = 148, g = 0, b = 211 }
-        Menu.Banner.imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png"
+        Menu.Banner.imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png"
         Menu.CurrentTheme = "Purple"
     elseif themeLower == "gray" then
         Menu.Colors.HeaderPink = { r = 128, g = 128, b = 128 }
         Menu.Colors.SelectedBg = { r = 128, g = 128, b = 128 }
-        Menu.Banner.imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png"
+        Menu.Banner.imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png"
         Menu.CurrentTheme = "Gray"
     elseif themeLower == "pink" then
         Menu.Colors.HeaderPink = { r = 255, g = 20, b = 147 }
         Menu.Colors.SelectedBg = { r = 255, g = 20, b = 147 }
-        Menu.Banner.imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png"
+        Menu.Banner.imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png"
         Menu.CurrentTheme = "pink"
     else
         Menu.Colors.HeaderPink = { r = 148, g = 0, b = 211 }
         Menu.Colors.SelectedBg = { r = 148, g = 0, b = 211 }
-        Menu.Banner.imageUrl = "https://i.postimg.cc/FFVVZm13/image-(1).png"
+        Menu.Banner.imageUrl = "https://i.postimg.cc/g0GY3v7y/image-629x178.png"
         Menu.CurrentTheme = "Purple"
     end
 
@@ -3012,6 +3012,7 @@ if Menu.Banner.enabled and Menu.Banner.imageUrl then
 end
 
 return Menu
+
 
 
 
